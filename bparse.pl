8a9a93f5929d2396cc807063631ea536dc0547a7
khafir <ogmogul@gmail.com>
2016-04-14 14:49:18 -0500

    Added files via upload

--- bparse.pl
+++ bparse.pl
@@ -1,14 +1,17 @@
 #!/usr/bin/perl -w
 use strict;
 
+#11.04.2016 08:34:05: |TG| LowSpeed (2.97.118.225:2304) 7cff65011259b4f59b40640dddef3b92 - #28 "bject) nearObjects ["Land_Communication_anchor_F",12];{_x setDamage 1} forEach _anchorList;"
+
 if (!$ARGV[0]){print "\nUse: ./bparse.pl full_path_to_battleye_scripts_log_file\nex:  ./barse.pl /home/steam/steamcmd/arma3/battleye/scripts.log\n\n";exit;}
 my $file = "$ARGV[0]";
 
 my $str;
-my (@parsed,@chopped);
+my (@parsed,@chopped,@udat);
 my @raw = `cat $file`;
 foreach (@raw)
 {
+	push @udat, "$1,$2,$3,$4" if ($_ =~ m/^(.*)\:\s+(.*)\s+\((.*)\)\s+(.*)\s+\-\s+\#/);
 	if ($_ =~ m/\s(\#\d+)\s+(.*\")$/){chomp($_);push @parsed, "$1,$2";next;}
 	if ($_ =~ m/\s(\#\d+)\s+(.*)$/){chomp($_);$str = "$1,$2";next;}
 	if ($_ =~ m/^\n|\r$/)
@@ -67,6 +70,36 @@ foreach (@chopped)
 	}
 }
 
+
+
+#foreach (@chopped){print "$_\n";}
+#my $pct = scalar(@chopped);print "\nTotal Entries parsed from $file: $pct\n\n";
+
+#foreach (@udat){print "$_\n";}
+#my $uct = scalar(@udat);print "\nLines in \@udat: $uct\n\n";
+
+
+#11.04.2016 08:34:05: |TG| LowSpeed (2.97.118.225:2304) 7cff65011259b4f59b40640dddef3b92 - #28 "bject) nearObjects ["Land_Communication_anchor_F",12];{_x setDamage 1} forEach _anchorList;"
+
+
+my $clt = 0;
+
+
+foreach my $line (@udat)
+{
+	my ($date,$user,$ip,$guid);
+	my $pre = "";
+	if ($ARGV[1])
+	{
+		if ($ARGV[1] =~ m/\-d.*d.*/i){($date,$user,$ip,$guid) = split(/,/,$udat[$line]);$pre = "$pre"."$date,";}
+		if ($ARGV[1] =~ m/\-d.*i.*/i){($date,$user,$ip,$guid) = split(/,/,$udat[$line]);$pre = "$pre"."$ip,";}
+		if ($ARGV[1] =~ m/\-d.*u.*/i){($date,$user,$ip,$guid) = split(/,/,$udat[$line]);$pre = "$pre"."$user,";}
+		if ($ARGV[1] =~ m/\-d.*g.*/i){($date,$user,$ip,$guid) = split(/,/,$udat[$line]);$pre = "$pre"."$guid,";}
+		if ($ARGV[1] =~ m/\-d.*a.*/i){($date,$user,$ip,$guid) = split(/,/,$udat[$line]);$pre = "$date,"."$ip,"."$user,"."$guid,";}
+		$chopped[$clt] = "$pre"."$chopped[$clt]";
+	}
+	$clt++;
+}
+
 foreach (@chopped){print "$_\n";}
 my $pct = scalar(@chopped);print "\nTotal Entries parsed from $file: $pct\n\n";
-
